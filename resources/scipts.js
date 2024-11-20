b791fe9459a59b2eb1950f13c71040a257a805e6
Yusuf <yusufolanrewaju68@gmail.com>
2024-11-20 11:14:30 +0100

    .

--- resources/scipts.js
+++ resources/scipts.js
@@ -1,18 +1,43 @@
-const typed = new Typed('#multiple', {
-    strings: ["Frontend Developer", "Flutter Developer", "Software Developer", "Data Entry Specialist", "Graphics Designer"],
-    typeSpeed: 50,
-    backSpeed: 50,
-    backDelay: 1000,
-    loop: true
-});
-
+let tabs = document.querySelectorAll('.tab');
 let navbar = document.querySelector(".navbar");
 let hamburger = document.querySelector("#hamburger");
 let tasks = document.querySelector(".services-done");
 let content = document.querySelector(".content");
 let main = document.querySelector(".main");
 let skills = document.querySelector(".skills");
-let footer = document.querySelector("footer")
+let footer = document.querySelector("footer");
+
+
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(t => t.classList.remove('active'));
+        tab.classList.add('active');
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const activeTab = document.querySelector('.tab.active');
+    
+    if (activeTab) {
+        activeTab.classList.add('active');
+
+        const targetSection = document.querySelector(activeTab.getAttribute('href'));
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+});
+
+
+const typed = new Typed('#multiple', {
+    strings: ["Frontend Developer", "Flutter Developer", "Software Developer", "Data Entry Specialist", "Graphic Designer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
 
 hamburger.addEventListener('click', () => {
     navbar.classList.toggle('display');
@@ -35,6 +60,11 @@ skills.addEventListener('click', ()=> {
 })
 
 footer.addEventListener('click', ()=> {
+    content.style.display = "none";
+    navbar.classList.remove('display');
+})
+
+other.addEventListener('click', ()=> {
     content.style.display = "none";
     navbar.classList.remove('display');
 })
