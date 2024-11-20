86c9986c238a981d81d84853c03ab2cefcfa9b07
Yusuf <yusufolanrewaju68@gmail.com>
2024-11-20 13:31:23 +0100

    .

--- resources/scipts.js
+++ resources/scipts.js
@@ -52,19 +52,27 @@ tasks.addEventListener('click', ()=> {
 main.addEventListener('click', ()=> {
     content.style.display = "none";
     navbar.classList.remove('display');
+    hamburger.classList.remove('rotate');
+    hamburger.classList.add('rotate2');
 })
 
 skills.addEventListener('click', ()=> {
     content.style.display = "none";
     navbar.classList.remove('display');
+    hamburger.classList.remove('rotate');
+    hamburger.classList.add('rotate2');
 })
 
 footer.addEventListener('click', ()=> {
     content.style.display = "none";
     navbar.classList.remove('display');
+    hamburger.classList.remove('rotate');
+    hamburger.classList.add('rotate2');
 })
 
 other.addEventListener('click', ()=> {
     content.style.display = "none";
     navbar.classList.remove('display');
+    hamburger.classList.remove('rotate');
+    hamburger.classList.add('rotate2');
 })
